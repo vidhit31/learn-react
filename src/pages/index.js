8e87303108cfde925f80fac94ccc82837030472f
Vidhi Thacker <46989276+vidhit31@users.noreply.github.com>
2023-12-07 19:28:41 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,14 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import Profile from './components/profile.js'
+import Profile from './components/profile_props.js'
+import Firstcomp from "./qcomps/firstcomp";
 
 export default function Home() {
   return (
     <div className={styles.main}>
         <Profile />
+        <Firstcomp />
     </div>
   )
 }
